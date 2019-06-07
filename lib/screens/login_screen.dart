afa2b5ef61e9c3d5ceeb36c51b4ca64c15cd114e
fadli <fadlimuharram@hotmail.com>
2019-06-07 09:37:12 +0700

    init

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flash_chat/components/RoundedButton.dart';
+import 'package:flash_chat/constants.dart';
+
+class LoginScreen extends StatefulWidget {
+  static String id = 'login_screen';
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Hero(
+              tag: 'logo',
+              child: Container(
+                height: 200.0,
+                child: Image.asset('images/logo.png'),
+              ),
+            ),
+            SizedBox(
+              height: 48.0,
+            ),
+            TextField(
+              onChanged: (value) {
+                //Do something with the user input.
+              },
+              decoration:
+                  KTextFieldDecoration.copyWith(hintText: 'Enter Email'),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              onChanged: (value) {
+                //Do something with the user input.
+              },
+              decoration:
+                  KTextFieldDecoration.copyWith(hintText: 'Enter Password'),
+            ),
+            SizedBox(
+              height: 24.0,
+            ),
+            RoundedButton(
+              onPress: () {},
+              text: 'Log In',
+              color: Colors.lightBlueAccent,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
